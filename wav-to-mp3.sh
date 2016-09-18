c469abe4502c0fcfb4c944af05febe088ca55cb7
toXel <timo625@gmail.com>
2016-09-18 22:55:00 +0200

    wav-to-mp3: Fix iterating through files

--- wav-to-mp3.sh
+++ wav-to-mp3.sh
@@ -35,7 +35,7 @@ then
   mkdir "${WATCH_PATH}/original"
 fi
 
-for file in "${WATCH_PATH}/*.wav"
+for file in $WATCH_PATH/*.wav
 do
   ffmpeg -i "${file}" -acodec libmp3lame -aq $QUALITY "${file}.mp3"
   mv "${file}" "${WATCH_PATH}/original/"
