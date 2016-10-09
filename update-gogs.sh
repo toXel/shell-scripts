93b80ae80b7cb647fff1bacbb17883691d01b3e3
toXel <timo625@gmail.com>
2016-10-09 23:09:59 +0200

    Add update-gogs.sh script

---
+++ update-gogs.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+cd /home/$USER/
+
+echo "Download new gogs version..."
+wget "https://dl.gogs.io/gogs_latest_linux_amd64.tar.gz" -O gogs_current.tar.gz
+
+echo "Remove old gogs-backup..."
+rm -r gogs-backup
+
+echo "Create new gogs-backup folder..."
+mv gogs gogs-backup
+
+echo "Extract new gogs version..."
+tar xf gogs_current.tar.gz
+
+echo "Copy data from the old version..."
+cp -r gogs-backup/custom gogs/
+cp -r gogs-backup/data gogs/
+
+echo "Restart gogs instance..."
+svc -du service/gogs
+
+echo "Cleanup..."
+rm gogs_current.tar.gz
+
+echo "Done."
